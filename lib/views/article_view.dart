816cf7d4650fc98cd9bff92c934fdbfe690a1fa8
UCHENNA <ucj.justice@gmail.com>
2020-06-16 13:51:12 +0100

    inti commit

--- lib/views/article_view.dart
+++ lib/views/article_view.dart
@@ -1,34 +1,6 @@
 import 'dart:async';
-import 'package:firebase_admob/firebase_admob.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-
-import '../main.dart';
-
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  FirebaseAdMob.instance.initialize(appId: 'ca-app-pub-8204002520550586~6510917622');
-  runApp(MyApp());
-  myBanner
-  // typically this happens well before the ad is shown
-    ..load()
-    ..show(
-      // Positions the banner ad 60 pixels from the bottom of the screen
-      anchorOffset: 0.0,
-      // Positions the banner ad 10 pixels from the center of the screen to the right
-      horizontalCenterOffset: 0.0,
-      // Banner Position
-      anchorType: AnchorType.bottom,
-    );
-
-  myInterstitial
-    ..load()
-    ..show(
-      anchorType: AnchorType.bottom,
-      anchorOffset: 0.0,
-      horizontalCenterOffset: 0.0,
-    );
-}
 
 class ArticleView extends StatefulWidget {
 
@@ -81,22 +53,3 @@ class _ArticleViewState extends State<ArticleView> {
     );
   }
 }
-
-
-MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
-  keywords: <String>['flutterio', 'beautiful apps'],
-  contentUrl: 'https://flutter.io',
-  childDirected: false,
-  testDevices: <String>[], // Android emulators are considered test devices
-);
-
-InterstitialAd myInterstitial = InterstitialAd(
-  // Replace the testAdUnitId with an ad unit id from the AdMob dash.
-  // https://developers.google.com/admob/android/test-ads
-  // https://developers.google.com/admob/ios/test-ads
-  adUnitId: 'ca-app-pub-8204002520550586/8017651976',
-  targetingInfo: targetingInfo,
-  listener: (MobileAdEvent event) {
-    print("InterstitialAd event is $event");
-  },
-);
