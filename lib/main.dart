f9d34ff5d8b37db4e00a47ceafef926f132067ce
UCHENNA <ucj.justice@gmail.com>
2020-07-21 13:11:02 +0100

    inti commit

--- lib/main.dart
+++ lib/main.dart
@@ -53,7 +53,7 @@ BannerAd myBanner = BannerAd(
   // Replace the testAdUnitId with an ad unit id from the AdMob dash.
   // https://developers.google.com/admob/android/test-ads
   // https://developers.google.com/admob/ios/test-ads
-  adUnitId: 'ca-app-pub-8204002520550586/1404456793',
+  adUnitId: 'ca-app-pub-8204002520550586/8169293233',
   size: AdSize.smartBanner,
   targetingInfo: targetingInfo,
   listener: (MobileAdEvent event) {
@@ -65,7 +65,7 @@ InterstitialAd myInterstitial = InterstitialAd(
   // Replace the testAdUnitId with an ad unit id from the AdMob dash.
   // https://developers.google.com/admob/android/test-ads
   // https://developers.google.com/admob/ios/test-ads
-  adUnitId: 'ca-app-pub-8204002520550586/3647476759',
+  adUnitId: 'ca-app-pub-8204002520550586/2916966559',
   targetingInfo: targetingInfo,
   listener: (MobileAdEvent event) {
     print("InterstitialAd event is $event");
